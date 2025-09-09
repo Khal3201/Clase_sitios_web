863a64d496ed73e89ea0b5985cd8b743536fde2f
Khaleb Haziel Luis Reyes <khalrey320@gmail.com>
2025-09-09 08:43:52 -0600

    Primera practica en sql server

---
+++ Prácticas_primer_parcial/practica_bd_1.sql
@@ -0,0 +1,14 @@
+create database bdprueba;
+go
+
+
+
+create table usuarios (
+
+id_user int identity, 
+nombre varchar (50),
+email varchar (50),
+age int,
+nom_user varchar (50),
+pass varchar (50)
+)
